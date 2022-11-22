e54672704632d5f7f8598ddcec2770fb01669f3b
mtkdv <89004483+24matsuki@users.noreply.github.com>
2022-11-22 17:21:50 +0900

    ログイン画面の実装 Fixes #10

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import "../styles/globals.css";
 import type { ReactElement, ReactNode } from "react";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
+import { Auth } from "#/components/Auth";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -15,5 +17,10 @@ export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return (
+    <RecoilRoot>
+      <Auth />
+      {getLayout(<Component {...pageProps} />)}
+    </RecoilRoot>
+  );
 }
