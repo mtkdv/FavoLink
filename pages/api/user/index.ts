7b26c2576cb878c3cfe0fd069f4f337dd75551ac
mtkdv <89004483+24matsuki@users.noreply.github.com>
2022-12-04 17:40:27 +0900

    api/user/index.tsを追加

---
+++ pages/api/user/index.ts
@@ -0,0 +1,11 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+// POST /api/user
+// Required fields in body: id, name, image
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const result = await prisma.user.create({ data: req.body });
+  res.json(result);
+}
