3de407ac96f8e9161cc46ec0ad683fd515704acf
mtkdv <89004483+24matsuki@users.noreply.github.com>
2022-12-11 21:43:45 +0900

    fix: Closes #59
    user,imageカラムをProfileテーブルに追加。profile取得、追加、更新の処理を修正。

--- lib/useGetProfile.ts
+++ lib/useGetProfile.ts
@@ -2,16 +2,6 @@ import { Profile } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
 import { Session } from "next-auth";
 
-// export type Profile = {
-//   name?: string;
-//   image?: string;
-//   slug?: string;
-//   description?: string;
-//   published?: boolean;
-// };
-
-// const { data: profile } = useGetProfile<Profile>(session);
-
 // export const useGetProfile = <T>(session: Session | null) => {
 export const useGetProfile = (session: Session | null) => {
   const links = useQuery({
